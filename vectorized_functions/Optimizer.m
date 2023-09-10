ec4aeb82152cf9900fd2f30210c33ca0e11f971c
HyrumHansen <hyrum.hansen1@gmail.com>
2023-09-09 18:13:07 -0600

    caught edge cases, refactored design storage procedure

--- vectorized_functions/Optimizer.m
+++ vectorized_functions/Optimizer.m
@@ -3,7 +3,7 @@
 
 % Random initial vector
 N = 6;
-K = 2;
+K = 1;
 
 % Constraints
 lb = -ones(N*K, 1);
@@ -16,12 +16,12 @@ Aeq = [];
 beq = [];
 
 % Loop it and store the data
-iterations = 100;
+iterations = 30;
 
 run = double.empty(iterations, 0);
 spvs = double.empty(iterations, 0);
-designs = repmat({[]}, 1, iterations);
 f_evals = double.empty(iterations, 0);
+designs = {};
 
 
 parpool('local', 18); % Start a local pool of workers
@@ -51,14 +51,28 @@ parfor i = 1:iterations
     % Store my data
     run(i) = i;
     spvs(i) = fval;
-    designs{i} = reshape(x_optimal, N, K);
-    f_evals(i) = output.funcCount;  
+    f_evals(i) = output.funcCount; 
+    X_current = reshape(x_optimal, N, K);
+    
+    % Needed to store the designs
+    X_store = [];
+    for col = 1:K
+        my_col = [];
+        for row = 1:N
+            my_col = [my_col X_current(row, col)];
+        end
+        X_store = [X_store; my_col];
+    end
+    
+    % Finally to store the designs
+    designs{i} = X_store.'
+ 
 
 end
 toc
 data = table(run(:), spvs(:), f_evals(:));
 writetable(data, "collected_data/k=2_N=6_nelder_mead.csv")
-writecell(designs, "collected_data/k=2_N=6_nelder_mead_designs.csv")
+writecell(designs, "matrices.csv")
 
 
 
